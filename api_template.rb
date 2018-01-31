f98599da6d987d633e35541372d49fd67ef1742e
John Romas <jromas@trunkclub.com>
2018-01-30 22:29:27 -0600

    Update api template

--- api_template.rb
+++ api_template.rb
@@ -1,5 +1,5 @@
 gem 'active_model_serializers', '~> 0.10.0'
-gem 'devise_token_auth', branch: 'master'
+gem 'devise_token_auth'
 gem 'delayed_job_active_record'
 gem 'figaro'
 gem 'httparty'
@@ -10,28 +10,21 @@ gem_group :development, :test do
   gem 'pry'
 end
 
-# run "bundle exec rake rails:update:bin"
-# run "bundle exec figaro install"
-# run "rails g delayed_job:active_record"
-# run "rails g devise_token_auth:install User auth"
-
-# rails_command "db:migrate"
-
 application "
-  logger           = ActiveSupport::Logger.new(STDOUT)
-  logger.formatter = config.log_formatter
-  config.log_tags  = [:subdomain, :uuid]
-  config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.log_tags  = [:subdomain, :uuid]
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
 "
 environment "config.active_job.queue_adapter = :delayed_job"
 environment "
-    config.middleware.use Rack::Cors do
-      allow do
-        origins '*'
-        resource '*',
-          :headers => :any,
-          :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-          :methods => [:get, :post, :options, :delete, :put]
-      end
+  config.middleware.use Rack::Cors do
+    allow do
+      origins '*'
+      resource '*',
+        :headers => :any,
+        :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+        :methods => [:get, :post, :options, :delete, :put]
     end
+  end
 ", env: 'development'
