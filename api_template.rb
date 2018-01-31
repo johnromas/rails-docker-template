8cd4514b9fe46d7de73c71fb3f9429b563694699
John Romas <jromas@trunkclub.com>
2018-01-30 18:54:56 -0600

    Dont execute scripts from template

--- api_template.rb
+++ api_template.rb
@@ -1,24 +1,21 @@
-rails_command("db:create")
-
 gem 'active_model_serializers', '~> 0.10.0'
 gem 'devise_token_auth', branch: 'master'
 gem 'delayed_job_active_record'
 gem 'figaro'
 gem 'httparty'
 gem 'omniauth'
-gem 'pg'
 gem 'rack-cors'
 
 gem_group :development, :test do
   gem 'pry'
 end
 
-run "bundle exec rake rails:update:bin"
-run "bundle exec figaro install"
-run "rails g delayed_job:active_record"
-run "rails g devise_token_auth:install User auth"
+# run "bundle exec rake rails:update:bin"
+# run "bundle exec figaro install"
+# run "rails g delayed_job:active_record"
+# run "rails g devise_token_auth:install User auth"
 
-rails_command "db:migrate"
+# rails_command "db:migrate"
 
 environment "config.active_job.queue_adapter = :delayed_job"
 environment "
@@ -32,4 +29,3 @@ environment "
       end
     end
 ", env: 'development'
-
